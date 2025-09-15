dc78f9dd7bb76de2778eb355bb1c97ce53ec2c9d
phantomvines <lkrath@web.de>
2025-09-15 19:12:07 +0200

    fix planet clicking

--- rund-um-die-welten/main.gd
+++ rund-um-die-welten/main.gd
@@ -3,4 +3,5 @@ extends Node2D
 
 
 func _physics_process(delta: float) -> void:
-	print($"/root/GlobalVariables".target_planet_location)
+	#print($"/root/GlobalVariables".target_planet_location)
+	pass
--- rund-um-die-welten/planet/planet.gd
+++ rund-um-die-welten/planet/planet.gd
@@ -4,8 +4,9 @@ var center_position = Vector2.ZERO
 
 func _ready() -> void:
 	# set center position to rotate around
-	center_position = $center_position.position
+	center_position = $center_position.global_position
+	print(center_position)
 
 func _on_pressed() -> void:
 	# update main to 
-	$"/root/GlobalVariables".target_planet_position = center_position
+	$"/root/GlobalVariables".target_planet_location = center_position
