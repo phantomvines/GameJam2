7618e7a36534801519ab202f160d1c226abc4085
phantomvines <lkrath@web.de>
2025-09-17 11:58:21 +0200

    collected collectible, lavel name

--- rund-um-die-welten/collectibles/collectible.gd
+++ rund-um-die-welten/collectibles/collectible.gd
@@ -7,7 +7,8 @@ var level
 func _ready() -> void:
 	level = get_parent().scene_file_path.split("/")[-1].split(".")[0]
 	if GlobalVariables.collectibles[level][id] == 1:
-		queue_free()
+		$grayscale.visible = true
+		$colour.visible = false
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
