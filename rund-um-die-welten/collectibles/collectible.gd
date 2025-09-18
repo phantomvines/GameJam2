4c1ef39abd27048da5a519c2c0b7def9d0c30742
phantomvines <lkrath@web.de>
2025-09-18 13:15:24 +0200

    collectables updates

--- rund-um-die-welten/collectibles/collectible.gd
+++ rund-um-die-welten/collectibles/collectible.gd
@@ -10,6 +10,8 @@ func _ready() -> void:
 		$grayscale.visible = true
 		$colour.visible = false
 		$CPUParticles2D.emitting = false
+	
+	
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
