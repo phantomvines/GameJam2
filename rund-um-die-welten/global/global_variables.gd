c04303ab6b220ebb181c0a64b51b045ebe886d5b
phantomvines <lkrath@web.de>
2025-09-18 20:20:22 +0200

    updated todo

--- rund-um-die-welten/global/global_variables.gd
+++ rund-um-die-welten/global/global_variables.gd
@@ -15,7 +15,7 @@ var selected_level = ""
 var global_RNG := RandomNumberGenerator.new()
 
 # Level vars
-var collectibles = {"Level 1": [1,0,0], "Level 2": [1,0,0], "Level 3": [0,0,0], "Level 4": [0,0,0], "Level 5": [0,0,0], "Level 6": [0,0,0], "Level 7": [0,0,0], "Level 8": [0,0,0], "Level 9": [0,0,0], "Level 10": [0,0,0]}
+var collectibles = {"Level 1": [0,0,0], "Level 2": [0,0,0], "Level 3": [0,0,0], "Level 4": [0,0,0], "Level 5": [0,0,0], "Level 6": [0,0,0], "Level 7": [0,0,0], "Level 8": [0,0,0], "Level 9": [0,0,0], "Level 10": [0,0,0]}
 var active_buttons = {"red": false, "blue": false, "green": false}
 var levels = {"Level 1": 0, "Level 2": 0, "Level 3": 0, "Level 4": 0, "Level 5": 0, "Level 6": 0, "Level 7": 0, "Level 8": 0, "Level 9": 0, "Level 10": 0}
 
