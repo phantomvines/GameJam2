57dac6fd225fb20a7c5e37e81f270ec7ed3ce5d9
flixp <dont.disturb.me@outlook.de>
2025-09-18 15:14:11 +0200

    Skeletton for button utils

--- rund-um-die-welten/global/global_variables.gd
+++ rund-um-die-welten/global/global_variables.gd
@@ -13,12 +13,18 @@ var selected_level = ""
 #Doesnt work yet
 var global_RNG := RandomNumberGenerator.new()
 
+# Level vars
 var collectibles = {"Level 1": [1,0,0], "Level 2": [0,0,0], "Level 3": [0,0,0], "Level 4": [0,0,0], "Level 5": [0,0,0], "Level 6": [0,0,0], "Level 7": [0,0,0], "Level 8": [0,0,0], "Level 9": [0,0,0], "Level 10": [0,0,0]}
+var active_buttons = {"red": false, "blue": false, "green": false}
 
-#Signals
+# Signals
+# Game-State-Signals
 signal player_died(death_message: String)
 signal game_over(game_over_message: String)
 signal level_done(win_message: String)
+
+# Other Signals
+signal button_changed_stage(button_name: String)
 
 func _ready() -> void:
 	global_RNG.seed = 12345
@@ -31,7 +37,6 @@ func emit_game_over(game_over_message: String):
 	game_over.emit(game_over_message)
 	
 func emit_level_done(win_message: String):
-	print("win")
 	level_done.emit(win_message)
 	
 
@@ -46,3 +51,22 @@ func restart_level():
 func change_level(level_select_path):
 	get_tree().change_scene_to_file(level_select_path)
 	restart_level()
+
+
+# Button utils
+func reset_all_buttons():
+	for i in active_buttons:
+		active_buttons[i] = false
+	button_changed_stage.emit("all")
+
+func set_button(button_name: String):
+	active_buttons[button_name] = true
+	button_changed_stage.emit(button_name)
+	
+func reset_button(button_name: String):
+	active_buttons[button_name] = false
+	button_changed_stage.emit(button_name)
+	
+func toggle_button(button_name: String):
+	active_buttons[button_name] = !active_buttons[button_name]
+	button_changed_stage.emit(button_name)
