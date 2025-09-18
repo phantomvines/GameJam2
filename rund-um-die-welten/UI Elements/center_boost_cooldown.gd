4371e9ed26f7ea6a0e117940798fa4826cf74b51
phantomvines <lkrath@web.de>
2025-09-18 16:45:40 +0200

    scene utils

--- rund-um-die-welten/UI Elements/center_boost_cooldown.gd
+++ rund-um-die-welten/UI Elements/center_boost_cooldown.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 func _physics_process(delta: float) -> void:
-	var timer = get_parent().get_node("player/boost_cooldown")
+	var timer = get_parent().get_parent().get_node("player/boost_cooldown")
 	$TextureProgressBar.max_value = timer.wait_time
 	
 	if timer.is_stopped():
