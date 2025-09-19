3f8dce4ab84f8e0a2ace20912a572ae28fca5429
Kurma9 <misterlm@me.com>
2025-09-19 11:35:45 +0200

    new laser and level

--- rund-um-die-welten/global/global_variables.gd
+++ rund-um-die-welten/global/global_variables.gd
@@ -9,7 +9,7 @@ var player_skin = "default"
 # Planet vars
 enum planets {DeathStar, Mars, Earth, Sun, Soap, Plant}
 var planet_names = {planets.DeathStar: "Death Star", planets.Mars: "Mars", planets.Earth: "Earth", planets.Sun: "Sun", planets.Soap: "Soap", planets.Plant: "Plant"}
-var selected_level = ""
+var selected_level = "Level 1"
 
 #Doesnt work yet
 var global_RNG := RandomNumberGenerator.new()
