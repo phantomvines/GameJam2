dbb8afeaf849d4f77c16893fb38a81a32e3dfd3e
phantomvines <lkrath@web.de>
2025-09-19 15:04:35 +0200

    audio

--- rund-um-die-welten/LevelSelectUtility/back_to_galaxy_button.gd
+++ rund-um-die-welten/LevelSelectUtility/back_to_galaxy_button.gd
@@ -9,6 +9,7 @@ func _ready():
 func _on_pressed():
 	var level_select_path = rootfolder + subfolder +"Main_Menu" + ".tscn"
 	if FileAccess.file_exists(level_select_path):
+		Audioplayer.play_sound((load("res://sfx/button_clicks.wav") as AudioStream))
 		GlobalVariables.change_level(level_select_path, true)
 	else :
 		print("File not found under " + level_select_path)
