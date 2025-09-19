ea13b5e58edee22295743b31cdc9b14a68645bd2
flixp <dont.disturb.me@outlook.de>
2025-09-19 14:54:29 +0200

    added text

--- rund-um-die-welten/LevelSelectUtility/world_select_star_system.gd
+++ rund-um-die-welten/LevelSelectUtility/world_select_star_system.gd
@@ -4,7 +4,10 @@ var clickable_area_radius = 140:
 	set(value):
 		clickable_area_radius = value
 		$collision_shape.shape.radius = clickable_area_radius
-@export var world_file_name: String
+@export var world_file_name: String:
+	set(value):
+		world_file_name = value
+		$world_name_text.text = world_file_name
 func _ready() -> void:
 		$collision_shape.shape.radius = clickable_area_radius
 		input_pickable = true
